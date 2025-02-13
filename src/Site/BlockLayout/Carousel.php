78f6fcd98d1056edfd48e21d7e497a42abe6b980
Thibaud Guillot <thibaud.guillot@biblibre.com>
2025-02-13 16:42:47 +0100

    fix cs

--- src/Site/BlockLayout/Carousel.php
+++ src/Site/BlockLayout/Carousel.php
@@ -1,8 +1,6 @@
 <?php
 namespace ItemCarouselBlock\Site\BlockLayout;
 
-use Laminas\Form\Element;
-use Laminas\Form\Form;
 use Laminas\View\Renderer\PhpRenderer;
 use Omeka\Api\Representation\SitePageBlockRepresentation;
 
